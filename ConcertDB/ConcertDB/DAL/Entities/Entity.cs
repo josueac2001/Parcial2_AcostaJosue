5479614eee80c52de8c8296751dd891f0c0eb37a
JOSU� ACOSTA LONDO�O <josueacosta261853@correo.itm.edu.co>
2023-04-01 17:44:50 -0500

    Entity Tickets and DatabaseContext created

---
+++ ConcertDB/ConcertDB/DAL/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
+{
+    public class Entity
+    {
+        #region Properties
+        [Key] //PRIMARY KEY
+        [Required] //NOT NULL
+        public Guid Id { get; set; }
+
+        [Display(Name = "Fecha de uso")]
+        public DateTime? UseDate { get; set; }
+        [Display(Name = "Fue Usada?")]
+        public Boolean IsUsed { get; set; }
+        [Display(Name = "Porteria")]
+        public String EntranceGate { get; set; }
+        #endregion
+    }
+}
---
+++ ConcertDB/ConcertDB/DAL/Entities/Tickets.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
+{
+    public class Tickets : Entity
+    {
+        #region Properties
+        [Display(Name = "Fecha de uso")]
+        public DateTime? UseDate { get; set; }
+        [Display(Name = "Fue Usada?")]
+        public Boolean IsUsed { get; set; }
+        [Display(Name = "Porteria")]
+        public String? EntranceGate { get; set; }
+        #endregion
+
+        #region Methods
+        public List<string> EntranceGateOptions { get; } = new List<string>
+        {
+            "Norte",
+            "Sur",
+            "Oriental",
+            "Occidental",
+        };
+        #endregion
+    }
+}
